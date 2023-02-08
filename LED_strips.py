9b283b62dd95b252ee85c77f3487dbcc9fbaed96
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-08 14:33:38 +0100

    1212

--- LED_strips.py
+++ LED_strips.py
@@ -44,9 +44,9 @@ class LED_strips:
         # Comment this line out if you have RGBW/GRBW NeoPixels
         #pixels.fill((0, 255, 0))
         # Uncomment this line if you have RGBW/GRBW NeoPixels
-        pixels.fill((0, 0, 0, 0))
+        #pixels.fill((0, 0, 0, 0))
         pixels.show()
-        time.sleep(1)
+        time.sleep(0.1)
 
         # Comment this line out if you have RGBW/GRBW NeoPixels
         #pixels.fill((0, 0, 255))
