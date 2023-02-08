4a04a455c9cbd518a5e5d0a7efce9a8229ad30a8
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-08 16:31:27 +0100

    121212

--- main.py
+++ main.py
@@ -282,7 +282,7 @@ class CustomDialog(QDialog):
                 self.buttonMap[key] = QPushButton(key)
                 self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
                 buttonsLayout.addWidget(self.buttonMap[key], row, col)
-                self.buttonMap[key].connect(self.showww)
+                self.buttonMap[key].clicked.connect(self.showww)
         self.layout.addLayout(buttonsLayout)
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
