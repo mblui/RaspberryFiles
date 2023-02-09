8effebfbc4381d69d259f9ee9f121025b3ebc538
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-02-09 16:57:36 +0100

    12121

--- lightsettingsClass.py
+++ lightsettingsClass.py
@@ -56,8 +56,8 @@ class lightsettingsClass:
         self.w.SliderVal_but_text_blue.setText(str(RGB_value[2]))
 
             ## Slider Brightness 
-        self.w.slider_red.setMinimum(0)
-        self.w.slider_red.setMaximum(100)
+        self.w.slider_slider_intensity.setMinimum(0)
+        self.w.slider_slider_intensity.setMaximum(100)
         self.w.slider_intensity.setValue(Brightness)
         self.w.SliderVal_but_text_intensity.setText(str(Brightness))
 
@@ -99,6 +99,6 @@ class lightsettingsClass:
         self.w.SliderVal_but_text_blue.setText(str(RGB_value[2]))
 
             ## Slider Brightness 
-        #self.w.slider_intensity.setValue(Brightness)
+        self.w.slider_intensity.setValue(Brightness)
         print("HEHEHE", Brightness)
         self.w.SliderVal_but_text_intensity.setText(str(Brightness)+"%")
