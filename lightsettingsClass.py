11617405429041490bfb92b2d77b24ae639da696
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-03-02 15:45:31 +0100

    12121

--- lightsettingsClass.py
+++ lightsettingsClass.py
@@ -123,8 +123,8 @@ class lightsettingsClass:
         if returnvalue:
             # Write Setting to txt file
             RGB_valllll = np.array([self.w.slider_red.value() ,self.w.slider_green.value(), self.w.slider_blue.value()])
-            self.print_on_GUI_terminal(text_to_print=str(RGB_valllll)+str(Brighdhfdf),  color='default')
             Brighdhfdf = self.w.slider_intensity.value()
+            self.print_on_GUI_terminal(text_to_print=str(RGB_valllll)+str(Brighdhfdf),  color='default')
             text_to_print = str(loaded_light_profile) + "; profile " + str(loaded_light_profile) + "; RGB;" + str(RGB_valllll) + "; BRIGHTNESS; [" + str(Brighdhfdf) + "] \n"
             lightsettingsClass.replace_line('/home/dgslr/ProgramFiles/LightProfiles.txt', 2, text_to_print)
             self.print_on_GUI_terminal(text_to_print=" Settings stored in file!",  color='default')
