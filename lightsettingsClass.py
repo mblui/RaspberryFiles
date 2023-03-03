2ae819ec34465b6833d8ff3ec25cbf0298de2ee7
Mart Bluiminck VS <m.bluiminck@dgs-ps.nl>
2023-03-03 08:28:58 +0100

    12121

--- lightsettingsClass.py
+++ lightsettingsClass.py
@@ -108,11 +108,11 @@ class lightsettingsClass:
         previous_key_val = profile.exec() 
         if previous_key_val:
             lines = open('/home/dgslr/ProgramFiles/LightProfiles_saved.txt', 'r').readlines()
-            print("hoi", lines[int(profile.previous_key)+2][20:23])
-            R_val = int(lines[int(profile.previous_key)+2][20:23])            
-            G_val = int(lines[int(profile.previous_key)+2][24:27])
-            B_val = int(lines[int(profile.previous_key)+2][28:31])            
-            BB_val = int(lines[int(profile.previous_key)+2][48:51]) 
+            print("hoi", lines[int(profile.previous_key)+2][22:25])
+            R_val = int(lines[int(profile.previous_key)+2][22:25])            
+            G_val = int(lines[int(profile.previous_key)+2][26:29])
+            B_val = int(lines[int(profile.previous_key)+2][30:33])            
+            BB_val = int(lines[int(profile.previous_key)+2][50:53]) 
             self.print_on_GUI_terminal(text_to_print="Light profile: " + str(profile.previous_key) + " is succesfully loaded!",  color='black')
             loaded_light_profile = profile.previous_key
             self.w.text_loaded_profile.setText(str(loaded_light_profile))
